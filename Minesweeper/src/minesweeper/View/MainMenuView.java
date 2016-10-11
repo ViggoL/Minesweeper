3798e599d76ea7396c3ecf025276bf58b896bf7a
Johan Lipecki <lipecki@kth.se>
2016-10-11 23:54:29 +0200

    knappar med padding

--- Minesweeper/src/minesweeper/View/MainMenuView.java
+++ Minesweeper/src/minesweeper/View/MainMenuView.java
@@ -18,6 +18,9 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.scene.*;
 import javafx.*;
+import javafx.geometry.Insets;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.text.Font;
 import minesweeper.Controller.MainMenuController;
 
@@ -30,32 +33,50 @@ public class MainMenuView {
     public Button playButton;
     public Button settingsButton;
     public Button rulesButton;
+    public VBox buttonPane;
+    public Pane emptyPane1, emptyPane2;
     
     public MainMenuView()
     {
         playButton = new Button();
         settingsButton = new Button();
         rulesButton = new Button();
+        buttonPane = new VBox();
+        emptyPane1 = new Pane();
+        emptyPane2 = new Pane();
     }
     
     public void update(Stage primaryStage)
     {
+        Insets buttonInsets = new Insets(5);
+        
         Label title = new Label("Minesweeper");
         title.setFont(new Font("Helvetica", 30));
-        
+        title.setPadding(new Insets(5,5,20,5));
         
         playButton.setText("Play");
         playButton.setAlignment(Pos.CENTER);
+        
         settingsButton.setText("Settings");
+        
         rulesButton.setText("Rules");
+        
+        emptyPane1.setPadding(buttonInsets);
+        emptyPane2.setPadding(buttonInsets);
+        
+        buttonPane.getChildren().addAll(playButton, emptyPane1, settingsButton, emptyPane2 , rulesButton);
+        buttonPane.setAlignment(Pos.CENTER);
+        buttonPane.setPadding(buttonInsets);
+        
         VBox root = new VBox();
         root.setAlignment(Pos.TOP_CENTER);
-        root.getChildren().addAll(title, playButton, settingsButton, rulesButton);
+        root.getChildren().addAll(title, buttonPane);
         
-        Scene scene = new Scene(root, 300, 250);
+        Scene scene = new Scene(root, 300, 240);
 
         primaryStage.setTitle("Minesweeper");
         primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
     
