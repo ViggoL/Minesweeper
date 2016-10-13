ff5707efcf33ff295b6babc9afea283c1f8d842a
ViggoL <viggo.lunden@gmail.com>
2016-10-13 16:58:39 +0200

    PauseButton

---
+++ Minesweeper/src/minesweeper/View/PauseButton.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package minesweeper.View;
+
+import javafx.scene.control.Button;
+
+/**
+ *
+ * @author Viggo
+ */
+public class PauseButton extends GameButton {
+
+    public PauseButton(){
+        super();
+    }
+    
+    @Override
+    public Button setButton(double diameter) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
